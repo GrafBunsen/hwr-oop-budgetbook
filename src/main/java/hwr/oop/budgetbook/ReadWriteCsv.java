5bd5bfc51384a641e571aca6d63b0b581599b3fe
Malte Eisermann <online@eisermann.xyz>
2022-05-09 10:28:34 +0200

    Created class for ConsoleOutput and moved printTable Method to new class

--- src/main/java/hwr/oop/budgetbook/ReadWriteCsv.java
+++ src/main/java/hwr/oop/budgetbook/ReadWriteCsv.java
@@ -40,47 +40,6 @@ public class ReadWriteCsv {
         csvWriter.close();
     }
 
-    public void printTable(String[][] table) {
-        int longestCell = getLongestCell(table);
-        String[][] output = makeAllCellsTheSameLength(table, longestCell);
-        printTheCleanedOutput(output);
-    }
-
-    private void printTheCleanedOutput(String[][] output) {
-        for (String[] strings : output) {
-            System.out.print("| ");
-            for (String string : strings) {
-                System.out.print(string + " | ");
-            }
-            System.out.println();
-        }
-    }
-
-    private String[][] makeAllCellsTheSameLength(String[][] output, int longestCell) {
-        for (int i = 0; i < output.length; i++) {
-            for (int j = 0; j < output[i].length; j++) {
-                while (output[i][j].length() < longestCell) {
-                    output[i][j] = output[i][j] + " ";
-                }
-            }
-        }
-        return output;
-    }
-
-    private int getLongestCell(String[][] table) {
-        int longestCell = 0;
-
-        for (String[] strings : table) {
-            for (String string : strings) {
-                if (longestCell < string.length()) {
-                    longestCell = string.length();
-                }
-            }
-
-        }
-        return longestCell;
-    }
-
     private String[][] append(String[][] oldArray, String[] newLine) {
         int oldArrayLength = oldArray.length;
         int newArrayLength = oldArrayLength + 1;
--- src/test/java/hwr/oop/budgetbook/ReadWriteCsvTest.java
+++ src/test/java/hwr/oop/budgetbook/ReadWriteCsvTest.java
@@ -9,9 +9,10 @@ public class ReadWriteCsvTest {
     void readCsvFile_TryIfATestFileIsReadAndDisplayed() {
         String path = "C:\\Users\\Malte\\IdeaProjects\\hwr-oop-project-bank\\src\\test\\java\\hwr\\oop\\budgetBook\\test.csv";
         ReadWriteCsv test = new ReadWriteCsv();
+        ConsoleOutput output = new ConsoleOutput();
         try {
             String[][] data = test.readCsvFile(path);
-            test.printTable(data);
+            output.printTable(data);
         } catch (Exception e) {
             e.printStackTrace();
             System.out.println("Error");
@@ -19,13 +20,15 @@ public class ReadWriteCsvTest {
     }
 
     @Test
-    void writeCsvFile_WriteACsvFileWithATestDataset(){
-        String[] dataset = new String[] {"doe78","1234","John","Doe"};
+    void writeCsvFile_WriteACsvFileWithATestDataset() {
+        String[] dataset = new String[]{"doe78", "1234", "John", "Doe"};
         String path = "C:\\Users\\Malte\\IdeaProjects\\hwr-oop-project-bank\\src\\test\\java\\hwr\\oop\\budgetBook\\test1.csv";
 
         ReadWriteCsv test = new ReadWriteCsv();
+        ConsoleOutput output = new ConsoleOutput();
+
         try {
-            test.writeDataset(dataset,path);
+            test.writeDataset(dataset, path);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -35,7 +38,7 @@ public class ReadWriteCsvTest {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        test.printTable(data);
+        output.printTable(data);
 
     }
 
