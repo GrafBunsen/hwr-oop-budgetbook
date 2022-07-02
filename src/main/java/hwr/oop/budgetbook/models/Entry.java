edb6952bfd1ee827a4e553543777328ba6655d50
martincodes-de <martin.schneider@techniknews.net>
2022-07-02 14:43:50 +0200

    add getter and setter

--- src/main/java/hwr/oop/budgetbook/models/Entry.java
+++ src/main/java/hwr/oop/budgetbook/models/Entry.java
@@ -14,4 +14,44 @@ public class Entry {
         category = newCategory;
         description = newDescription;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
