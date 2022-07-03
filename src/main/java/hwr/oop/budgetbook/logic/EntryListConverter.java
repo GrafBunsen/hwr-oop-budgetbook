b7f5ab3c423b7ab1f83e3c7af1678c2524eeaac8
martincodes-de <martin.schneider@techniknews.net>
2022-07-03 20:34:23 +0200

    add  methods to converter

--- src/main/java/hwr/oop/budgetbook/logic/EntryListConverter.java
+++ src/main/java/hwr/oop/budgetbook/logic/EntryListConverter.java
@@ -2,6 +2,7 @@ package hwr.oop.budgetbook.logic;
 
 import hwr.oop.budgetbook.models.Entry;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -21,5 +22,27 @@ public class EntryListConverter {
                 Integer.parseInt(fileLine.get(0)), Integer.parseInt(fileLine.get(1)),
                 Integer.parseInt(fileLine.get(2)), fileLine.get(3), fileLine.get(4)
         );
+    }
+
+    public List<List<String>> convertEntries(HashMap<Integer, Entry> entryList) {
+        List<List<String>> convertedList = new ArrayList<>();
+
+        entryList.forEach((id, entry) -> {
+            convertedList.add(convertToList(entry));
+        });
+
+        return convertedList;
+    }
+
+    private List<String> convertToList(Entry entry) {
+        List<String> list = new ArrayList<>();
+
+        list.add(String.valueOf(entry.getId()));
+        list.add(String.valueOf(entry.getDate()));
+        list.add(String.valueOf(entry.getAmount()));
+        list.add(entry.getCategory());
+        list.add(entry.getDescription());
+
+        return list;
     }
 }
